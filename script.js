5e852ce13311fd42ece5d40aa20e665fea17b66f
Denise Oliveira da Cruz <denise.ocrz@gmail.com>
2020-11-30 22:25:11 -0300

    .

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Javascript carregado");
+
+
+
+    function validacao() {
+        console.log("Iniciando validação CPF"); 
+    }  
+
+        var cpf = document.getElementById("cpf_digitado").value; 
+        
+        var resultadoValidacao = validaCPF(cpf);
+
+        if (resultadoValidacao) {
+            document.getElementById("success").style.display = "block";
+        } else {
+            document.getElementById("error").style.display = "block";
+
+     }
